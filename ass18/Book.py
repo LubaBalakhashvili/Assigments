d5513dd6e5cbb5e0e375786ff84149382c8816da
LubaBalakhashvili <102228184+LubaBalakhashvili@users.noreply.github.com>
2024-02-12 00:21:23 +0400

    Add files via upload

---
+++ ass18/Book.py
@@ -0,0 +1,19 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __eq__(self, other):
+        if isinstance(other, Book):
+            return self.title == other.title and self.author == other.author
+        else:
+            raise TypeError("Not 'Book' type")
+
+
+# Example usage:
+book1 = Book('1984', 'George Orwell')
+book2 = Book('1984', 'George Orwell')
+book3 = Book('Brave New World', 'Aldous Huxley')
+
+print(book1 == book2)  # Output: True
+print(book1 == book3)  # Output: False
---
+++ ass18/Vector.py
@@ -0,0 +1,21 @@
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        if isinstance(other, Vector):
+            new_x = self.x + other.x
+            new_y = self.y + other.y
+            return Vector(new_x, new_y)
+        else:
+            raise TypeError("Not 'Vector' type")
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+# Example usage:
+v1 = Vector(2, 3)
+v2 = Vector(3, 4)
+v3 = v1 + v2
+print(v3)  # Output: (5, 7)
